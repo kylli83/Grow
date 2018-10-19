7d3c5519789ae4b59ff09340aa02c656cb43b2c0
kylli <kyllikool@gmail.com>
2018-10-19 15:20:58 +0300

    irrelevant trainings

--- src/com/helmes/grow/TrainingsService.java
+++ src/com/helmes/grow/TrainingsService.java
@@ -22,16 +22,23 @@ public class TrainingsService implements ITrainingsService {
 	}
 
 	@Override
-	public List<Training> findIrrelevant(String jobTitle, List<Training> trainings) {
-		return null;
-	}
-
-	public List<String> convertListToUpperCase(List<String> stringList) {
+		public List<String> convertListToUpperCase(List<String> stringList) {
 		List<String> result = new ArrayList<>();
 		for (String s: stringList) {
 			result.add(s.toUpperCase());
 		}
 		return result;
 	}
-
+    @Override
+    public List<Training> findIrrelevant(String jobTitle, List<Training> trainings) {
+        List<Training> irrelevantresult = new ArrayList<>();
+        for (Training t : trainings) {
+            List<String> jobTitles = t.getRelatedJobTitles();
+            if (!convertListToUpperCase(jobTitles).contains(jobTitle.toUpperCase())){
+                continue;
+            }
+            irrelevantresult.add(t);
+        }
+        return irrelevantresult;
+}
 }
